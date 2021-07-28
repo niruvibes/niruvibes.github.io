07b5c196afd1dd84f06e1550bc3ffeec6ad6ea65
niru.vibes <67180137+niruvibes@users.noreply.github.com>
2021-07-28 13:02:57 +1000

    scroll stuff idk

--- assets/js.js
+++ assets/js.js
@@ -1,9 +1,18 @@
-var nystories = document.querySelector(maininfo).offsetTop;
+/*
+var nystories = document.querySelector("p").offsetTop;
 window.onscroll = function() {
   if (window.pageYOffset > 0) {
  var opac = (window.pageYOffset / nystories);
     console.log(opac);
-  document.header.style.background = "linear-gradient(rgba(255, 255, 255, " + opac + "), rgba(255, 255, 255, " + opac + ")), url(bg4.gif) no-repeat";
+  document.body.style.background = "linear-gradient(rgba(225, 225, 225, " + opac + "), rgba(225, 225, 225, " + opac + ")), url(assets/bg4.gif) no-repeat";
+  document.body.style.backgroundSize = "100%";
   }
-}
+} */
 
+window.onscroll = function(ev) {
+  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+     setTimeout(function() {
+        location.href = "/skills";
+     }, 10)
+  }
+};
