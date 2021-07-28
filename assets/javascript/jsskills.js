d5e67469dfe6a9c97121c7f3fac766ae914f2819
niru.vibes <67180137+niruvibes@users.noreply.github.com>
2021-07-28 14:17:27 +1000

    fixed scroll

--- assets/javascript/jsskills.js
+++ assets/javascript/jsskills.js
@@ -1,5 +1,5 @@
 window.onscroll = function(ev) {
-    if ((window.innerHeight + window.scrollY) <= document.body.offsetHeight) {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
        setTimeout(function() {
           location.href = "/portfolio.html";
        }, 10)
