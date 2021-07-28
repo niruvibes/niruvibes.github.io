a4889e8b1a25c717ee1135e75b788b0e3704bcd7
niru.vibes <67180137+niruvibes@users.noreply.github.com>
2021-07-28 13:37:58 +1000

    portfolio scroll

--- assets/js.js
+++ assets/js.js
@@ -16,3 +16,11 @@ window.onscroll = function(ev) {
      }, 10)
   }
 };
+
+window.onscroll = function(ev) {
+  if ((window.innerHeight + window.scrollY) <= document.body.offsetHeight) {
+     setTimeout(function() {
+        location.href = "/portfoilio.html";
+     }, 10)
+  }
+};
